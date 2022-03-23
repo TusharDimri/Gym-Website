f805b952575483b90d00a99a4063026ae0d1ea2d
Tushar <tushar.dimri22@gmail.com>
2022-03-23 22:06:38 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const app = express();
+const port = 3000;
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.get("/", (req, res)=>{
+    res.sendFile(__dirname+"/static/index.html");   
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
